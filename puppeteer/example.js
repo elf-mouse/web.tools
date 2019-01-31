8ddd155ed48d6722c49364ac25252be816435a84
Elf-mousE <ifmouz@gmail.com>
2019-01-31 11:40:55 +0800

    update

---
+++ puppeteer/example.js
@@ -0,0 +1,13 @@
+/**
+ * Example - navigating to https://example.com and saving a screenshot as example.png
+ */
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://example.com');
+  await page.screenshot({ path: 'example.png' });
+
+  await browser.close();
+})();
